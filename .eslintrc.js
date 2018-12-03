2f5359e228a2bc731f69e9c593626429ff9c421a
RyoNkmr <ryonakamuraryo@gmail.com>
2018-12-03 19:21:38 +0900

    prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = {
   createApi: require('./modules/api'),
-};
+}
